5fee33a56af7f34b796a819ec13d141d3e470193
Jim Rollenhagen <jim@jimrollenhagen.com>
2019-09-26 10:47:57 -0400

    Retire github mirror, repo moved to opendev

--- deployment_scripts/puppet/manifests/site-compute-post.pp
+++
@@ -1,1 +0,0 @@
-class {'vxlan::compute':}
--- deployment_scripts/puppet/modules/vxlan/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'vxlan' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('vxlan') }
-  end
-end
